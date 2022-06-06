f75e3b084d26a1a0d392d776c6363771ad07f3e7
Peter Panovski <saschafischer2406@gmail.com>
2022-06-06 19:56:23 +0200

    Learning by doing
    doing while learning

---
+++ public_html/Formsproject/forms.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>forms-php</title>
+    </head>
+    <body>
+      Vorname: <?php echo $_POST["Vorname"]; ?><br>
+      Nachname: <?php  echo $_POST("Nachname"); ?><br>
+    </body>
+</html>
---
+++ public_html/Javascript/scripts.js
@@ -0,0 +1,6 @@
+
+function grundlegendes_extern()
+    {
+     document.getElementById("Externe_funktion_p").innerHTML="geändert durch externe Funktion";
+    }
+
